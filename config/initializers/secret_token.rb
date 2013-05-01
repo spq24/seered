7df8c640a715ffb2666dbbab44122995879d3340
Steve Quatrani <spq2461@gmail.com>
2013-05-01 00:52:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seered::Application.config.secret_token = '952b96b733cb7fe5980a24a7a751d319ee7ce3df1a3ad14019516f49661ad84eac65a8d40cd8d6634af38dc8c95658e3b2436723035f918c5f071b1faae18ee1'
